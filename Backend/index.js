32443daa41c832fff1821111727329c1403adbfa
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2025-07-31 17:02:11 +0530

    🛠️ Connected MongoDB using Mongoose + backend changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+const app = express()
 dotenv.config();
 const PORT= process.env.PORT || 5001;
-const app = express()
+
 const port = 5002
+const URI = process.env.MONGODB_URI;
+
+try {
+  mongoose.connect(URI)
+  .then(console.log("MongoDB Connected "));
+}
+catch (error) {
+  console.log(error);
+
+  
+}
+
+
+
+
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
+   
