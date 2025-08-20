08cbfa200be5ffcc5ff84b583cfd1550e6592653
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2025-08-20 23:54:46 +0530

    design sign up and login page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,17 +2,23 @@ import React from 'react'
 import Left from './home/left/left'
 import Right from './home/left/right/Right'
 import Logout from './home/left/left1/Logout'
+import Signup from './components/signup'
+import Login from './components/Login'
 
 function App() {
   return (
     <>
-    <div className='flex h-screen'>
-      <Logout></Logout>
-    <Left></Left>
-    <Right></Right>
-    
-    </div>
-    </>
+                 { /*<div className='flex h-screen'>
+                    <Logout></Logout>
+                  <Left></Left>
+                  <Right></Right>
+                </div> 
+                 <Login/>
+                */}
+
+               
+<Signup/>
+              </>
   )
 }
 
